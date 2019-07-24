4c455843109b7b9990c9d57e573458fd40c973b6
Katusiime Joel Ian <joelethan@users.noreply.github.com>
2019-07-24 11:18:36 +0300

    Add error handling when sending a reset link

--- src/app/services/password-reset.service.ts
+++ src/app/services/password-reset.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PasswordResetService {
   data;
+  _error;
   url = 'http://127.0.0.1:8000/api/v1/auth/password-reset/';
-  // url = 'http://127.0.0.1:8000/api/v1/properties/';
+
 
   constructor(private http: HttpClient) { }
 
@@ -15,6 +18,11 @@ export class PasswordResetService {
     this.data = {
       email: _email
     }
-    return this.http.post(this.url, this.data);
+    return this.http.post(this.url, this.data)
+                    .pipe(catchError(this.errorHandler));
+  }
+  errorHandler(error: HttpErrorResponse) {
+    this._error = error.error.errors.email[0];
+    return throwError('A new error has occured: ' + this._error)
   }
 }
