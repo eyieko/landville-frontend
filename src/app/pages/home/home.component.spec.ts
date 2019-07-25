a38f88f2c50fb3c249344764f3c0823804f7eefc
James Mudidi <mudidi.jimmy@gmail.com>
2019-07-25 10:15:19 +0300

    ft(landing page):add landing page
    add a landing page for the application
    
    [Finishes #167373681]

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -21,26 +21,4 @@ describe('HomeComponent', () => {
     fixture.detectChanges();
   });
 
-
-  it('testing form the wrong way', () => {
-    component.onSubmit({value: {}, valid: false});
-    expect(component.form.submitted).toEqual(false);
-  });
-
-  it('testing form the right way', () => {
-    const testForm = {
-      value: {
-        firstName: 'alpha',
-        lastName: 'landville',
-        email: 'alpha@gmail.com',
-      },
-      valid: true,
-    };
-    component.onSubmit(testForm);
-    expect(component.user.email).toEqual(testForm.value.email);
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
