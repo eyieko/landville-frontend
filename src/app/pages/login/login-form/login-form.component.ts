d6366b8c987f29cf8b36c5502acbc9a2219eb7eb
Brian Serem <brianseremiat@gmail.com>
2019-07-31 12:29:33 +0300

    bug(image folder) : fix image folders

--- src/app/pages/login/login-form/login-form.component.ts
+++ src/app/pages/login/login-form/login-form.component.ts
@@ -8,7 +8,7 @@ import { LoginService } from 'src/app/services/login/login.service';
 
 @Component({
   selector: 'app-login-form',
-  templateUrl: '/login-form.component.html',
+  templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent implements OnInit {
