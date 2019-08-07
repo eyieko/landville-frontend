c9a4b4fb1e7fc7baec2ff095c20ca1c4265b7bc3
p@rick <frzpete@gmail.com>
2019-08-07 21:52:11 +0300

    Add validations for each of the fields required

--- src/app/pages/payments/payments.component.ts
+++ src/app/pages/payments/payments.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { InternationalPaymentService } from "../../services/internationalPayment/international-payment.service";
 import { ToastrService } from "ngx-toastr";
 import {
   FormGroup,
   FormBuilder,
   Validators,
-  ReactiveFormsModule
+  ReactiveFormsModule,
+  AbstractControl
 } from "@angular/forms";
+// import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-payments",
@@ -22,28 +24,55 @@ export class PaymentsComponent implements OnInit {
   constructor(
     private internationalPaymentService: InternationalPaymentService,
     private toastrService: ToastrService,
+    // private subscription: Subscription,
     private fb: FormBuilder
   ) {}
 
   ngOnInit() {
     this.form = this.fb.group({
-      cardno: [""],
-      cvv: [""],
-      expirymonth: [""],
-      expiryyear: [""],
-      amount: [""],
-      billingzip: [""],
-      billingcity: [""],
-      billingaddress: [""],
-      billingstate: [""],
-      billingcountry: [""],
-      save_card: [""],
-      purpose: [""]
+      cardno: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern("^[1-9]+[0-9]*$"),
+          Validators.minLength(8)
+        ]
+      ],
+      cvv: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern("^[1-9]+[0-9]*$"),
+          Validators.maxLength(4),
+          Validators.minLength(3)
+        ]
+      ],
+      expirymonth: [
+        null,
+        [Validators.required, Validators.max(12), Validators.min(1)]
+      ],
+      expiryyear: [null, [Validators.required, Validators.min(2019)]],
+      amount: [null, [Validators.required]],
+      billingzip: [null, [Validators.required]],
+      billingcity: [null, [Validators.required]],
+      billingaddress: [null, [Validators.required]],
+      billingstate: [null, [Validators.required]],
+      billingcountry: [null, [Validators.required]],
+      save_card: [null, [Validators.required]],
+      purpose: [null, [Validators.required]]
     });
   }
 
+  // ngOnDestroy() {
+  //   this.subscription.unsubscribe();
+  // }
+
+  // validateCardNo(control: AbstractControl): {[key: string]: boolean} | null{
+  //     if( typeof(control.value)
+  //     return null;
+  // }
+
   onSubmitPaymentDetails({ value }) {
-    // this.formData = this.form.value;
     console.log(value);
 
     this.loading = true;
@@ -61,6 +90,7 @@ export class PaymentsComponent implements OnInit {
       save_card: value.save_card,
       purpose: value.purpose
     };
+
     this.internationalPaymentService
       .createInternationalPayment(this.payload)
       // .pipe(first())
