6d6dd8dcc64b9c27147edbf36007fca8b5e70889
alpha Nganga <alphandirangu@gmail.com>
2019-08-08 13:00:33 +0300

    - Resolved Merge conflicts.
    [Finishes #167416901]

--- src/app/layouts/auth-layout/auth-layout.module.ts
+++ src/app/layouts/auth-layout/auth-layout.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import {
+  AuthService,
+  AuthServiceConfig,
+  FacebookLoginProvider,
+  GoogleLoginProvider,
+  SocialLoginModule
+} from 'angularx-social-login';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { AuthLayoutRoutes } from './auth-layout.routing';
-import { LoginComponent } from 'src/app/pages/login/login.component';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { APPCONFIG } from 'src/app/config';
 import { LoginFormComponent } from 'src/app/pages/login/login-form/login-form.component';
 import { LoginHeaderComponent } from 'src/app/pages/login/login-header/login-header.component';
 import { LoginSliderComponent } from 'src/app/pages/login/login-slider/login-slider.component';
-import { RegistrationComponent } from 'src/app/pages/registration/registration.component';
+import { LoginComponent } from 'src/app/pages/login/login.component';
 import { RegisterFormComponent } from 'src/app/pages/registration/register-form/register-form.component';
 import { RegisterHeaderComponent } from 'src/app/pages/registration/register-header/register-header.component';
 import { HomeComponent } from 'src/app/pages/home/home.component';
@@ -24,8 +32,9 @@ import {
   FacebookLoginProvider,
   AuthService
 } from 'angularx-social-login';
+import { RegistrationComponent } from 'src/app/pages/registration/registration.component';
 import { SocialLoginComponentt } from 'src/app/pages/SocialAuth/socialauth.component';
-import { APPCONFIG } from 'src/app/config';
+import { AuthLayoutRoutes } from './auth-layout.routing';
 
 // configs
 const config = new AuthServiceConfig([
@@ -38,9 +47,11 @@ const config = new AuthServiceConfig([
     provider: new FacebookLoginProvider(APPCONFIG.facebookId)
   }
 ]);
+
 export function provideConfig() {
   return config;
 }
+
 @NgModule({
   imports: [
     CommonModule,
@@ -61,7 +72,6 @@ export function provideConfig() {
     RegistrationComponent,
     RegisterFormComponent,
     RegisterHeaderComponent,
-    HomeComponent,
     SocialLoginComponentt
   ],
   providers: [
@@ -72,4 +82,5 @@ export function provideConfig() {
     }
   ]
 })
-export class AuthLayoutModule {}
+export class AuthLayoutModule {
+}
