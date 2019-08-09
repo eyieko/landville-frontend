5d6fdc4810a3099cab471159a9c93b5a3fd73960
p@rick <frzpete@gmail.com>
2019-08-09 11:06:14 +0300

    Add more tests

--- src/app/pages/payments/payments.component.spec.ts
+++ src/app/pages/payments/payments.component.spec.ts
@@ -5,12 +5,17 @@ import { FormsModule, ReactiveFormsModule, NgForm } from "@angular/forms";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { ToastrService } from "ngx-toastr";
 import { of, throwError } from "rxjs";
+import { Location } from "@angular/common";
+
 import {
   internationalPaymentServiceSpy,
   resetSpies,
   routerSpy,
-  toastServiceSpy
+  toastServiceSpy,
+  locationSpy
 } from "../../helpers/spies";
+import { InternationalPaymentService } from "src/app/services/internationalPayment/international-payment.service";
+import { By } from "@angular/platform-browser";
 
 describe("PaymentsComponent", () => {
   let component: PaymentsComponent;
@@ -20,7 +25,14 @@ describe("PaymentsComponent", () => {
     TestBed.configureTestingModule({
       imports: [FormsModule, ReactiveFormsModule, HttpClientTestingModule],
       declarations: [PaymentsComponent],
-      providers: [{ provide: ToastrService, useValue: toastServiceSpy }]
+      providers: [
+        { provide: ToastrService, useValue: toastServiceSpy },
+        { provide: Location, useValue: locationSpy },
+        {
+          provide: InternationalPaymentService,
+          useValue: internationalPaymentServiceSpy
+        }
+      ]
     }).compileComponents();
   }));
 
@@ -40,13 +52,44 @@ describe("PaymentsComponent", () => {
     expect(component.form.invalid).toBeTruthy();
   });
 
-  
-  it("should trigger onSubmitPaymentDetails method with invalid form", async(() => {
-    const form = {
-      value: {},
-      invalid: true
-    } as NgForm;
-    component.onSubmitPaymentDetails(form);
-    expect(component.form.invalid).toBeTruthy();
-  }));
+  it("should call submit", () => {
+    const response = {
+      message:
+        "https://ravesandboxapi.flutterwave.com/mockvbvpage?ref=FLW-MOCK-f3e901600b6dc73df17f7056a4f782de&code=00&message=Approved. Successful&receiptno=RN1565337244749",
+      txRef: "LANDVILLE-2019-08-09 07:54:01.760701"
+    };
+
+    internationalPaymentServiceSpy.createInternationalPayment.and.returnValue(
+      of(response)
+    );
+    const de = fixture.debugElement.query(By.css("form"));
+    de.triggerEventHandler("ngSubmit", response);
+    expect(toastServiceSpy.success).toHaveBeenCalledWith(
+      "Your Payment has been placed"
+    );
+  });
+
+  it("should call submit and throw an error", () => {
+    const responseError = {
+      purpose: ["This field is required."]
+    };
+
+    const value = {
+      amount: "44444"
+    };
+    internationalPaymentServiceSpy.createInternationalPayment.and.returnValue(
+      throwError(responseError)
+    );
+    component.onSubmitPaymentDetails({
+      value
+    });
+    expect(component.loading).toEqual(false);
+  });
+
+  it("should call backClicked Method", () => {
+    component.backClicked();
+    fixture.whenStable().then(() => {
+      expect(component.backClicked).toHaveBeenCalled();
+    });
+  });
 });
