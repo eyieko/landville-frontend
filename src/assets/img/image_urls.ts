342ac227d1e76cda4ccb45448b83af03ba9e3bde
kdaglas <daglach7@gmail.com>
2019-08-30 08:02:58 +0300

    fix(heroku build): change the url location of the required images

---
+++ src/assets/img/image_urls.ts
@@ -0,0 +1,10 @@
+export const image_urls = {
+  visa_card_url:
+    'https://res.cloudinary.com/landville/image/upload/v1566848151/FRONTEND_ASSETS/IMAGES/jrlki6prt2mzkycpw5gs.png',
+  master_card_url:
+    'https://res.cloudinary.com/landville/image/upload/v1566848502/FRONTEND_ASSETS/IMAGES/avonpeekaf3wopdi537n.png',
+  american_express_card:
+    'https://res.cloudinary.com/landville/image/upload/v1566848548/FRONTEND_ASSETS/IMAGES/o9mfhliozqf7al306qvr.jpg',
+  paypal_card:
+    'https://res.cloudinary.com/landville/image/upload/v1566848536/FRONTEND_ASSETS/IMAGES/jvxg2fodh7qyq0eg1zaf.jpg'
+};
