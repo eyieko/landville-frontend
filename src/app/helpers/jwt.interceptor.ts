d85af40780d161b5271c8033684a1fa65031d0ef
crycetruly <crycetruly@gmail.com>
2019-09-02 11:24:00 +0300

    Fix the absolute import issue

--- src/app/helpers/jwt.interceptor.ts
+++ src/app/helpers/jwt.interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 
 @Injectable({
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,11 +1,11 @@
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
+import { environment } from 'src/environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-export { AppServerModule } from './app/app.server.module';
+export { AppServerModule } from 'src/app/app.server.module';
 export { ngExpressEngine } from '@nguniversal/express-engine';
 export { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
